2a5b02d8edf09af4ca2abb88e4d03e28b5903d17
Alexander <alexander-bond@hotmail.com>
2016-07-13 15:00:58 +0200

    .

---
+++ src/main/java/de/thb/ue/backend/util/DSLParser.java
@@ -0,0 +1,13 @@
+package de.thb.ue.backend.util;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
+/**
+ * Created by Alexander on 13.07.2016.
+ */
+public class DSLParser {
+}
