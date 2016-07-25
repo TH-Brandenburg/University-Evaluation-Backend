674fa956afff914814ff85584f2dc3d25a9ea221
Alexander <alexander-bond@hotmail.com>
2016-07-25 15:00:10 +0200

    .

--- src/main/java/de/thb/ue/backend/util/DSLParser.java
+++ src/main/java/de/thb/ue/backend/util/DSLParser.java
@@ -10,4 +10,66 @@ import java.util.Scanner;
  * Created by Alexander on 13.07.2016.
  */
 public class DSLParser {
+    public static void main(String... aArgs) throws IOException {
+        DSLParser parser = new DSLParser("D:\\test.txt");
+        parser.processLineByLine();
+        log("Done.");
+    }
+
+    /**
+     Constructor.
+     @param aFileName full name of an existing, readable file.
+     */
+    public DSLParser(String aFileName){
+        fFilePath = Paths.get(aFileName);
+    }
+
+
+    /** Template method that calls {@link #processLine(String)}.  */
+    public final void processLineByLine() throws IOException {
+        try (Scanner scanner =  new Scanner(fFilePath, ENCODING.name())){
+            while (scanner.hasNextLine()){
+                processLine(scanner.nextLine());
+            }
+        }
+    }
+
+    protected void processLine(String aLine){
+        //use a second Scanner to parse the content of each line
+        Scanner scanner = new Scanner(aLine);
+        scanner.useDelimiter(":");
+
+        if (scanner.hasNext()){
+            String entry;
+            String value;
+            String q;
+            String a;
+            String[] answers;
+            entry = scanner.next();
+            value = scanner.next();
+            String firstPart = quote(entry.trim());
+            String secondPart = quote(value.trim());
+            answers = secondPart.split(";");
+
+            log(firstPart + " Text is: " + secondPart + Arrays.toString(answers));
+
+        }
+
+        else {
+            log("Empty or invalid line. Unable to process.");
+        }
+        //scanner.close();
+    }
+
+    private final Path fFilePath;
+    private final static Charset ENCODING = StandardCharsets.UTF_8;
+
+    private static void log(Object aObject){
+        System.out.println(String.valueOf(aObject));
+    }
+
+    private String quote(String aText){
+        String QUOTE = "'";
+        return QUOTE + aText + QUOTE;
+    }
 }
